093050eb530c7145ae48ad883339b7d6c956d6d0
Denis Cornehl <denis@cornehl.org>
2026-08-31 15:00:01 +0200

    add support for `pubtime` field in registry

--- tests/crates_index.rs
+++ tests/crates_index.rs
@@ -20,9 +20,9 @@ fn sizes() {
 
 #[test]
 fn semver() {
-    let c = Crate::from_slice(r#"{"vers":"1.0.0", "name":"test", "deps":[], "features":{}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234", "yanked":false}
-            {"vers":"1.2.0-alpha.1", "name":"test", "deps":[], "features":{}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234", "yanked":false}
-            {"vers":"1.0.1", "name":"test", "deps":[], "features":{}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234", "yanked":false}"#.as_bytes()).unwrap();
+    let c = Crate::from_slice(r#"{"vers":"1.0.0", "name":"test", "deps":[], "features":{}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234", "yanked":false, "pubtime":"2025-11-12T19:30:12Z"}
+            {"vers":"1.2.0-alpha.1", "name":"test", "deps":[], "features":{}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234", "yanked":false, "pubtime":"2025-11-12T19:30:12Z"}
+            {"vers":"1.0.1", "name":"test", "deps":[], "features":{}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234", "yanked":false, "pubtime":"2025-11-12T19:30:12Z"}"#.as_bytes()).unwrap();
     assert_eq!(c.most_recent_version().version(), "1.0.1");
     assert_eq!(c.highest_version().version(), "1.2.0-alpha.1");
     assert_eq!(c.highest_normal_version().unwrap().version(), "1.0.1");
@@ -30,7 +30,7 @@ fn semver() {
 
 #[test]
 fn features2() {
-    let c = Crate::from_slice(br#"{"vers":"1.0.0", "name":"test", "deps":[], "features":{"a":["one"], "b":["x"]},"features2":{"a":["two"], "c":["y"]}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234"}"#).unwrap();
+    let c = Crate::from_slice(br#"{"vers":"1.0.0", "name":"test", "deps":[], "features":{"a":["one"], "b":["x"]},"features2":{"a":["two"], "c":["y"]}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234", "pubtime":"2025-11-12T19:30:12Z"}"#).unwrap();
     let f2 = c.most_recent_version().features();
 
     assert_eq!(3, f2.len());
@@ -41,6 +41,22 @@ fn features2() {
 
 #[test]
 fn rust_version() {
-    let c = Crate::from_slice(br#"{"vers":"1.0.0", "name":"test", "deps":[], "features":{},"features2":{}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234", "rust_version":"1.64.0"}"#).unwrap();
+    let c = Crate::from_slice(br#"{"vers":"1.0.0", "name":"test", "deps":[], "features":{},"features2":{}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234", "rust_version":"1.64.0", "pubtime":"2025-11-12T19:30:12Z"}"#).unwrap();
     assert_eq!(c.most_recent_version().rust_version(), Some("1.64.0"));
+}
+
+#[test]
+fn pubtime() {
+    let c = Crate::from_slice(br#"{"vers":"1.0.0", "name":"test", "deps":[], "features":{}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234", "pubtime":"2025-11-12T19:30:12Z"}
+        {"vers":"1.0.1", "name":"test", "deps":[], "features":{}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234"}"#).unwrap();
+
+    assert_eq!(c.versions()[0].pubtime(), Some("2025-11-12T19:30:12Z"));
+    assert_eq!(c.versions()[1].pubtime(), None);
+}
+
+#[test]
+fn missing_pubtime_is_accepted() {
+    let result = Crate::from_slice(br#"{"vers":"1.0.0", "name":"test", "deps":[], "features":{}, "cksum":"1234567890123456789012345678901234567890123456789012345678901234"}"#);
+
+    assert!(result.is_ok());
 }
